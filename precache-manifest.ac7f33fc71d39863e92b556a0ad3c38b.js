10b9ab3f867a95d0b8628af456f802efe194feef
CAM <34009133+CMichael82@users.noreply.github.com>
2018-12-14 21:24:04 -0700

    Updates

---
+++ precache-manifest.ac7f33fc71d39863e92b556a0ad3c38b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "007f038ed33bf3e7c55c",
+    "url": "/clicky-game/static/js/main.007f038e.chunk.js"
+  },
+  {
+    "revision": "4c6dc1f2fde3e77db660",
+    "url": "/clicky-game/static/js/1.4c6dc1f2.chunk.js"
+  },
+  {
+    "revision": "007f038ed33bf3e7c55c",
+    "url": "/clicky-game/static/css/main.e5d20950.chunk.css"
+  },
+  {
+    "revision": "8eaa50f2e79935022a234856a2a1bd1a",
+    "url": "/clicky-game/index.html"
+  }
+];
